4e00aea79b7240d9cc59dd41732aad9bbac5ea23
vibber <mail@vibber.dk>
2014-05-14 15:27:04 +0200

    update webgl detector msg

--- js/libs/Detector-vib.js
+++ js/libs/Detector-vib.js
@@ -28,10 +28,11 @@ var Detector = {
 
 			element.innerHTML = window.WebGLRenderingContext ? [
 				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-				'Find out how to get it <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">here</a>.'
+				'For best performance I recommend using the Google Chrome browser.',
+				'or find out how to enable WebGL <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">here</a>.'
 			].join( '\n' ) : [
 				'Your browser does not seem to support WebGL.<br/>',
-				'For best performance I recommend using Google Chrome.',
+				'For best performance I recommend using the Google Chrome browser.',
 				'Or find out how to enable WebGl <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">here</a>.'
 			].join( '\n' );
 
